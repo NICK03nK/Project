2977216bbe4d9f23a8c6451249739388cd7a0e11
NICK03nK <nk03579008@gmail.com>
2023-10-11 22:44:02 +0800

    The user registration function front-end and back-end are written

--- Gobang_online/src/server.hpp
+++ Gobang_online/src/server.hpp
@@ -69,15 +69,125 @@ private:
         std::string method = req.get_method(); // 获取请求的方法
         std::string uri = req.get_uri(); // 获取请求的uri
 
-        std::string pathname = _web_root + uri;
-        std::string body;
-
-        // 将请求的静态资源数据读取到body中
-        file_util::read(pathname, body);
-
-        conn->set_status(websocketpp::http::status_code::ok); // 设置响应状态码
-        conn->set_body(body); // 设置http响应正文
+        if (method == "POST" && uri == "/reg")
+        {
+            return signup(conn);
+        }
+        else if (method == "POST" && uri == "/login")
+        {
+            return signin(conn);
+        }
+        else if (method == "GET" && uri == "/userinfo")
+        {
+            return info(conn);
+        }
+        else
+        {
+            return file_handler(conn);
+        }
     }
+
+private:
+    void http_resp(wsserver_t::connection_ptr& conn, bool result, const std::string& reason, websocketpp::http::status_code::value code)
+    {
+        Json::Value resp_json;
+
+        resp_json["result"] = result;
+        resp_json["reason"] = reason;
+
+        std::string resp_body;
+        json_util::serialize(resp_json, resp_body);
+
+        conn->set_status(code);
+        conn->set_body(resp_body);
+        conn->append_header("Content-Type", "application/json");
+    }
+
+    // 静态资源请求的处理
+    void file_handler(wsserver_t::connection_ptr& conn)
+    {
+        // 1.获取http请求的uri -- 资源路径，了解客户端请求的页面文件名称
+        websocketpp::http::parser::request req = conn->get_request(); // 获取http请求
+        std::string uri = req.get_uri(); // 获取请求的uri
+
+        // 2.组合出文件实际的路径(相对根目录 + uri)
+        std::string realpath = _web_root + uri;
+
+        // 3.如果请求的uri是个目录，则增加一个后缀 -- login.html
+        if (realpath.back() == '/')
+        {
+            realpath += "login.html";
+        }
+
+        // 4.读取文件内容；若文件不存在，则返回404
+        std::string body;
+        bool ret = file_util::read(realpath, body);
+        if (ret == false)
+        {
+            body += "<html>";
+            body += "<head>";
+            body += "<meta charset='UTF-8'/>";
+            body += "</head>";
+            body += "<body>";
+            body += "<h1> Not Found </h1>";
+            body += "</body>";
+
+            conn->set_status(websocketpp::http::status_code::value::not_found);
+            conn->set_body(body);
+            return;
+        }
+
+        // 5.设置响应正文
+        conn->set_status(websocketpp::http::status_code::value::ok);
+        conn->set_body(body);
+    }
+
+    // 用户注册功能请求的处理
+    void signup(wsserver_t::connection_ptr& conn)
+    {
+        websocketpp::http::parser::request req = conn->get_request();
+
+        // 1.获取http请求正文
+        std::string req_body = conn->get_request_body();
+
+        // 2.对正文进行反序列化，得到用户名和密码
+        Json::Value signup_info;
+        Json::Value resp_json;
+        bool ret = json_util::unserialize(req_body, signup_info);
+        if (ret == false) // 反序列化失败
+        {
+            DLOG("unserialize failed");
+
+            return http_resp(conn, false, "request body is incorrectly formatted", websocketpp::http::status_code::value::bad_request);
+        }
+
+        // 3.进行数据库的用户新增操作(成功返回200，失败返回400)
+        if (signup_info["username"].isNull() || signup_info["password"].isNull())
+        {
+            DLOG("username or password is incomplete");
+
+            return http_resp(conn, false, "username or password is empty", websocketpp::http::status_code::value::bad_request);;
+        }
+
+        ret = _ut.signup(signup_info); // 将用户名和密码新增至数据库中
+        if (ret == false)
+        {
+            DLOG("insert data into database failed");
+
+            return http_resp(conn, false, "The username has been used", websocketpp::http::status_code::value::bad_request);
+        }
+
+        // 成功将用户名和密码加入数据库
+        return http_resp(conn, true, "sign up successfull", websocketpp::http::status_code::value::ok);
+    }
+
+    // 用户登录功能请求的处理
+    void signin(wsserver_t::connection_ptr& conn)
+    {}
+
+    // 获取用户信息功能请求的处理
+    void info(wsserver_t::connection_ptr& conn)
+    {}
 
 private:
     std::string _web_root; // 静态资源根目录  ./wwwroot/  /register.html --> ./wwwroot/register.html
