79e71a5e0fabf8d12027197288ac40848107af55
NICK03nK <nk03579008@gmail.com>
2024-03-08 09:27:26 +0800

    test6

--- TcpServer/src/test/client6.cc
+++ TcpServer/src/test/client6.cc
@@ -3,42 +3,24 @@
 
 #include "../http/http.hpp"
 
-// int main()
-// {
-//     Socket cli_sock;
-//     cli_sock.CreateClient(8080, "127.0.0.1");
-
-//     std::string req = "PUT /1234.txt HTTP/1.1\r\nConnection: keep-alive\r\n";
-//     std::string body;
-//     Util::ReadFile("./hello.txt", &body);
-//     req += "Content-Length: " + std::to_string(body.size()) + "\r\n\r\n";
-
-//     assert(cli_sock.Send(req.c_str(), req.size()) != -1);
-//     assert(cli_sock.Send(body.c_str(), body.size()) != -1);
-
-//     char buf[1024] = {0};
-//     assert(cli_sock.Recv(buf, 1023) != -1);
-//     DBG_LOG("[%s]", buf);
-
-//     cli_sock.Close();
-
-//     return 0;
-// }
-
 int main()
 {
     Socket cli_sock;
     cli_sock.CreateClient(8080, "127.0.0.1");
+
     std::string req = "PUT /1234.txt HTTP/1.1\r\nConnection: keep-alive\r\n";
     std::string body;
     Util::ReadFile("./hello.txt", &body);
     req += "Content-Length: " + std::to_string(body.size()) + "\r\n\r\n";
+
     assert(cli_sock.Send(req.c_str(), req.size()) != -1);
     assert(cli_sock.Send(body.c_str(), body.size()) != -1);
+
     char buf[1024] = {0};
-    assert(cli_sock.Recv(buf, 1023));
+    assert(cli_sock.Recv(buf, 1023) != -1);
     DBG_LOG("[%s]", buf);
-    sleep(3);
+
     cli_sock.Close();
+
     return 0;
 }
