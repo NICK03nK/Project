911d8575d311edaff6b1cebbdb72574c0c5ca099
NICK03nK <nk03579008@gmail.com>
2024-04-25 21:38:54 +0800

    implement log macro module

---
+++ Gobang_online_AG/src/logger.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+#include <ctime>
+
+#define INF 0
+#define DBG 1
+#define ERR 2
+#define DEFAULT_LOG_LEVEL INF
+
+#define LOG(level, format, ...)                                                             \
+    do                                                                                      \
+    {                                                                                       \
+        if (DEFAULT_LOG_LEVEL > level)                                                      \
+            break;                                                                          \
+        time_t t = time(NULL);                                                              \
+        struct tm *lt = localtime(&t);                                                      \
+        char buf[32] = {0};                                                                 \
+        strftime(buf, 31, "%H:%M:%S", lt);                                                  \
+        fprintf(stdout, "[%s %s:%d] " format "\n", buf, __FILE__, __LINE__, ##__VA_ARGS__); \
+    } while (false)
+
+#define ILOG(format, ...) LOG(INF, format, ##__VAR_ARGS__)
+#define DLOG(format, ...) LOG(DBG, format, ##__VAR_ARGS__)
+#define ELOG(format, ...) LOG(ERR, format, ##__VAR_ARGS__)
