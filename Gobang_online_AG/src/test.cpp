c3e472db04cebd955921030f3ded6a8490c3fc9e
NICK03nK <nk03579008@gmail.com>
2024-05-06 09:55:47 +0800

    test db.hpp

--- Gobang_online_AG/src/test.cpp
+++ Gobang_online_AG/src/test.cpp
@@ -1,4 +1,5 @@
 #include "util.hpp"
+#include "db.hpp"
 
 void json_test()
 {
@@ -48,11 +49,64 @@ void file_test()
     std::cout << body << std::endl;
 }
 
+void db_test()
+{
+    user_table utb("127.0.0.1", "root", "", "online_gobang", 3306);
+    // ------------------------------测试注册用户
+    // Json::Value user1;
+    // user1["username"] = "凤梨兄";
+    // user1["password"] = "123123";
+
+    // utb.signup(user1);
+
+    // ------------------------------测试登录
+    // Json::Value loginUser;
+    // loginUser["username"] = "nKk";
+    // loginUser["password"] = "123123123";
+    // if (utb.login(loginUser)) std::cout << "true" << std::endl;
+
+    // std::cout << loginUser["id"].asInt() << std::endl;
+    // std::cout << loginUser["username"].asString() << std::endl;
+    // std::cout << loginUser["password"].asString() << std::endl;
+    // std::cout << loginUser["score"].asInt() << std::endl;
+    // std::cout << loginUser["total_count"].asInt() << std::endl;
+    // std::cout << loginUser["win_count"].asInt() << td::endl;
+    
+    // ------------------------------测试select by username
+    // Json::Value userInfo;
+    // // if (utb.select_by_username("凤梨兄", userInfo))
+    // if (utb.select_by_username("凤梨", userInfo))
+    // {
+    //     std::cout << userInfo["id"].asInt() << std::endl;
+    //     std::cout << userInfo["username"].asString() << std::endl;
+    //     std::cout << userInfo["score"].asInt() << std::endl;
+    //     std::cout << userInfo["total_count"].asInt() << std::endl;
+    //     std::cout << userInfo["win_count"].asInt() << std::endl;
+    // }
+
+    // ------------------------------测试select by id
+    // Json::Value userInfo;
+    // // if (utb.select_by_id(1, userInfo))
+    // if (utb.select_by_id(3, userInfo))
+    // {
+    //     std::cout << userInfo["id"].asInt() << std::endl;
+    //     std::cout << userInfo["username"].asString() << std::endl;
+    //     std::cout << userInfo["score"].asInt() << std::endl;
+    //     std::cout << userInfo["total_count"].asInt() << std::endl;
+    //     std::cout << userInfo["win_count"].asInt() << std::endl;
+    // }
+
+    // ------------------------------测试victory&defeat
+    // utb.victory(3);
+    utb.defeat(1);
+}
+
 int main()
 {
     // json_test();
     // string_test();
-    file_test();
+    // file_test();
+    db_test();
     
     return 0;
 }
