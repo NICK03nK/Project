cd30a4e36c71dcfcbd99d6e16b8a95ca50c03a3b
NICK03nK <nk03579008@gmail.com>
2024-05-07 20:30:45 +0800

    uniform naming format

--- Gobang_online_AG/src/online.hpp
+++ Gobang_online_AG/src/online.hpp
@@ -13,63 +13,63 @@ class online_manager
 {
 public:
     // websocket连接建立的时候，才会加入游戏大厅的在线用户管理
-    void enter_game_hall(uint64_t userId, websocketsvr_t::connection_ptr& conn)
+    void enter_game_hall(uint64_t user_id, websocketsvr_t::connection_ptr& conn)
     {
         std::unique_lock<std::mutex> lock(_mtx);
-        _hall_users.insert({userId, conn});
+        _hall_users.insert({user_id, conn});
     }
 
     // websocket连接建立的时候，才会加入游戏房间的在线用户管理
-    bool enter_game_room(uint64_t userId, websocketsvr_t::connection_ptr& conn)
+    bool enter_game_room(uint64_t user_id, websocketsvr_t::connection_ptr& conn)
     {
         std::unique_lock<std::mutex> lock(_mtx);
-        _room_users.insert({userId, conn});
+        _room_users.insert({user_id, conn});
     }
 
     // websocket连接断开的时候，才会移除游戏大厅的在线用户管理
-    bool exit_game_hall(uint64_t userId)
+    bool exit_game_hall(uint64_t user_id)
     {
         std::unique_lock<std::mutex> lock(_mtx);
-        _hall_users.erase(userId);
+        _hall_users.erase(user_id);
     }
 
     // websocket连接断开的时候，才会移除游戏房间的在线用户管理
-    bool exit_game_room(uint64_t userId)
+    bool exit_game_room(uint64_t user_id)
     {
         std::unique_lock<std::mutex> lock(_mtx);
-        _room_users.erase(userId);
+        _room_users.erase(user_id);
     }
 
     // 判断指定用户当前是否在游戏大厅中
-    bool is_in_game_hall(uint64_t userId)
+    bool is_in_game_hall(uint64_t user_id)
     {
         std::unique_lock<std::mutex> lock(_mtx);
-        if (_hall_users.find(userId) == _hall_users.end()) return false;
+        if (_hall_users.find(user_id) == _hall_users.end()) return false;
         return true;
     }
 
     // 判断指定用户当前是否在游戏房间中
-    bool is_in_game_room(uint64_t userId)
+    bool is_in_game_room(uint64_t user_id)
     {
         std::unique_lock<std::mutex> lock(_mtx);
-        if (_room_users.find(userId) == _room_users.end()) return false;
+        if (_room_users.find(user_id) == _room_users.end()) return false;
         return true;
     }
 
     // 通过用户id在游戏大厅用户管理中获取对应用户的通信连接
-    websocketsvr_t::connection_ptr get_conn_from_hall(uint64_t userId)
+    websocketsvr_t::connection_ptr get_conn_from_hall(uint64_t user_id)
     {
         std::unique_lock<std::mutex> lock(_mtx);
-        auto it = _hall_users.find(userId);
+        auto it = _hall_users.find(user_id);
         if (it == _hall_users.end()) return websocketsvr_t::connection_ptr();
         return it->second;
     }
 
     // 通过用户id在游戏房间用户管理中获取对应用户的通信连接
-    websocketsvr_t::connection_ptr get_conn_from_room(uint64_t userId)
+    websocketsvr_t::connection_ptr get_conn_from_room(uint64_t user_id)
     {
         std::unique_lock<std::mutex> lock(_mtx);
-        auto it = _room_users.find(userId);
+        auto it = _room_users.find(user_id);
         if (it == _room_users.end()) return websocketsvr_t::connection_ptr();
         return it->second;
     }
