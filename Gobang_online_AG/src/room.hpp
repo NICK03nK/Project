777edbdd9d18d77d3cc80c94e0faf136d7ac7b2f
NICK03nK <nk03579008@gmail.com>
2024-05-09 17:51:33 +0800

    1.fixed a bug chess was out of bounds  2.remove redundant code in sensitive words detection  3.implement the room manager class

--- Gobang_online_AG/src/room.hpp
+++ Gobang_online_AG/src/room.hpp
@@ -3,7 +3,11 @@
 #include <vector>
 #include <jsoncpp/json/json.h>
 #include <string>
-#include <unordered_set>
+#include <memory>
+#include <mutex>
+#include <unordered_map>
+#include <cstdlib>
+#include <ctime>
 
 #include "logger.hpp"
 #include "db.hpp"
@@ -76,7 +80,7 @@ public:
         int chess_col = req["col"].asInt();
 
         // 1. 判断走棋位置是否合理（是否越界，是否被占用）
-        if (chess_row > BOARD_ROWS || chess_col > BOARD_COLS)
+        if (chess_row >= BOARD_ROWS || chess_col >= BOARD_COLS)
         {
             resp["optype"] = "put_chess";
             resp["result"] = false;
@@ -303,7 +307,7 @@ private:
     // 敏感词检测
     bool have_sensitive_word(const std::string& msg)
     {
-        for (const auto& word : _words)
+        for (const auto& word : _sensitive_words)
         {
             // 聊天消息中包含敏感词
             if (msg.find(word) != std::string::npos) return true;
@@ -313,18 +317,17 @@ private:
     }
 
 private:
-    uint64_t _room_id;                             // 游戏房间id
-    room_statu _statu;                             // 游戏房间的状态
-    int _player_count;                             // 游戏房间中玩家的数量
-    uint64_t _white_id;                            // 白棋玩家的id
-    uint64_t _black_id;                            // 黑棋玩家的id
-    user_table* _user_table;                       // 数据库用户信息表的操作句柄
-    online_manager* _user_online;                  // 在线用户管理句柄
-    std::vector<std::vector<int>> _board;          // 棋盘
-    static std::vector<std::string> _words;        // 聊天敏感词（后期可补充）
-    static std::unordered_set<std::string> _dict;  // 聊天敏感词词库
+    uint64_t _room_id;                                // 游戏房间id
+    room_statu _statu;                                // 游戏房间的状态
+    int _player_count;                                // 游戏房间中玩家的数量
+    uint64_t _white_id;                               // 白棋玩家的id
+    uint64_t _black_id;                               // 黑棋玩家的id
+    user_table* _user_table;                          // 数据库用户信息表的操作句柄
+    online_manager* _user_online;                     // 在线用户管理句柄
+    std::vector<std::vector<int>> _board;             // 棋盘
+    static std::vector<std::string> _sensitive_words; // 聊天敏感词（后期可补充）
 };
-std::vector<std::string> room::_words = {"色情", "裸体", "性爱", "性交", "色情片",
+std::vector<std::string> room::_sensitive_words = {"色情", "裸体", "性爱", "性交", "色情片",
                                          "色情服务", "色情网站", "色情图片", "色情小说",
                                          "操", "滚", "傻逼", "蠢货", "贱人", "混蛋",
                                          "畜生", "白痴", "废物", "黑鬼", "黄种人", "白猪",
@@ -332,4 +335,134 @@ std::vector<std::string> room::_words = {"色情", "裸体", "性爱", "性交",
                                          "杀人", "打架", "战斗", "殴打", "刺杀", "爆炸",
                                          "恐怖袭击", "毒品", "赌博", "贩卖", "贿赂", "偷窃",
                                          "抢劫"};
-std::unordered_set<std::string> room::_dict(room::_words.begin(), room::_words.end());
+
+typedef std::shared_ptr<room> room_ptr;
+class room_manager
+{
+public:
+    room_manager(user_table* user_table, online_manager* user_online)
+        :_next_room_id(1)
+        , _user_table(user_table)
+        , _user_online(user_online)
+    {
+        srand(time(nullptr)); // 用于将玩家1和玩家2随机分配给白棋和黑棋
+        DLOG("房间管理类初始化完毕");
+    }
+
+    ~room_manager() { DLOG("房间管理类即将销毁"); }
+
+    room_ptr create_room(uint64_t user_id1, uint64_t user_id2)
+    {
+        // 两个玩家都在游戏大厅中匹配成功后才能创建房间
+        // 1. 判断玩家1和玩家2是否都在游戏大厅中
+        if (_user_online->is_in_game_hall(user_id1) == false) // 玩家1不在游戏大厅中
+        {
+            DLOG("创建游戏房间失败，玩家：%lu 不在游戏大厅中", user_id1);
+            return room_ptr();
+        }
+        if (_user_online->is_in_game_hall(user_id2) == false) // 玩家2不在游戏大厅中
+        {
+            DLOG("创建游戏房间失败，玩家：%lu 不在游戏大厅中", user_id2);
+            return room_ptr();
+        }
+
+        std::unique_lock<std::mutex> lock(_mtx); // 对下面的操作进行加锁保护
+        // 2. 创建房间，将用户信息添加到房间中
+        room_ptr proom(new room(_next_room_id++, _user_table, _user_online));
+        
+        // 玩家1和玩家2随机匹配白棋和黑棋
+        uint64_t white_id = rand() % 2 == 0 ? user_id1 : user_id2;
+        uint64_t black_id = white_id == user_id1 ? user_id2 : user_id1;
+        proom->add_white_player(white_id);
+        proom->add_black_player(black_id);
+
+        //-----------------------存疑？这里要不要调用_user_online->enter_game_room()？-----------------------
+
+        // 3. 将房间信息管理起来
+        _room_id_and_room.insert({proom->id(), proom});
+        _user_id_and_room_id.insert({user_id1, proom->id()});
+        _user_id_and_room_id.insert({user_id2, proom->id()});
+
+        return proom;
+    }
+
+    room_ptr get_room_by_room_id(uint64_t room_id)
+    {
+        std::unique_lock<std::mutex> lock(_mtx); // 对下面的操作进行加锁保护
+
+        auto it = _room_id_and_room.find(room_id);
+        if (it == _room_id_and_room.end()) // 没找到房间号为id的房间
+        {
+            DLOG("不存在房间id为：%d 的房间", room_id);
+            return room_ptr();
+        }
+
+        return it->second;
+    }
+
+    room_ptr get_room_by_user_id(uint64_t user_id)
+    {
+        std::unique_lock<std::mutex> lock(_mtx); // 对下面的操作进行加锁保护
+
+        auto it = _user_id_and_room_id.find(user_id);
+        if (it == _user_id_and_room_id.end())
+        {
+            DLOG("不存在与id为：%d 的玩家匹配的房间");
+            return room_ptr();
+        }
+
+        uint64_t room_id = it->second;
+        return get_room_by_room_id(room_id);
+    }
+
+    void remove_player_in_room(uint64_t user_id)
+    {
+        // 1. 通过玩家id获取游戏房间信息
+        room_ptr proom = get_room_by_user_id(user_id);
+        if (proom.get() == nullptr)
+        {
+            DLOG("通过玩家id获取游戏房间信息失败");
+            return;
+        }
+
+        // 2. 处理玩家退出房间动作
+        proom->handle_exit(user_id);
+
+        // 3. 判断游戏房间中是否还有玩家，没有则销毁游戏房间
+        if (proom->player_count() == 0) destroy_room(proom->id());
+    }
+
+    void destroy_room(uint64_t room_id)
+    {
+        // 1. 通过房间id获取游戏房间信息
+        room_ptr proom = get_room_by_room_id(room_id);
+        if (proom.get() == nullptr)
+        {
+            DLOG("通过房间id获取游戏房间信息失败");
+            return;
+        }
+
+        // 2. 通过游戏房间获取白棋玩家id和黑棋玩家id
+        uint64_t white_id = proom->get_white_player();
+        uint64_t black_id = proom->get_black_player();
+
+        {
+            std::unique_lock<std::mutex> lock(_mtx); // 加锁保护该作用域中的操作
+
+            // 3. 将白棋玩家和黑棋玩家在“玩家id和游戏房间id的关联关系”中移除
+            _user_id_and_room_id.erase(white_id);
+            _user_id_and_room_id.erase(black_id);
+
+            // 4. 将游戏房间信息从房间管理中移除
+            _room_id_and_room.erase(proom->id());
+        }
+    }
+
+private:
+    uint64_t _next_room_id;                                      // 房间id分配器
+    std::mutex _mtx;                                             // 互斥锁
+    user_table* _user_table;                                     // 数据库用户信息表的操作句柄
+    online_manager* _user_online;                                // 在线用户管理句柄
+    std::unordered_map<uint64_t, room_ptr> _room_id_and_room;    // 游戏房间id和游戏房间的关联关系
+    std::unordered_map<uint64_t, uint64_t> _user_id_and_room_id; // 玩家id和游戏房间id的关联关系
+};
