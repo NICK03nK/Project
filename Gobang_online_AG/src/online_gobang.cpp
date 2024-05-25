a7f7be635cc2d8797a67ad0d6b02537c71b2331e
NICK03nK <nk03579008@gmail.com>
2024-05-25 10:25:46 +0800

    set the game server as a daemon

--- Gobang_online_AG/src/online_gobang.cpp
+++ Gobang_online_AG/src/online_gobang.cpp
@@ -1,7 +1,15 @@
+#include <unistd.h>
+
 #include "server.hpp"
 
 int main()
 {
+    // 将游戏服务器设置为守护进程
+    if (daemon(1, 0) == -1) {
+        perror("daemon");
+        exit(EXIT_FAILURE);
+    }
+
     gobang_server server("127.0.0.1", "root", "", "online_gobang", 3306);
     server.start(8080);
 
