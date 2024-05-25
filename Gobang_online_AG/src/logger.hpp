5d3f7f8dcda82b87ec974edcd5c849d4f892b3ea
NICK03nK <nk03579008@gmail.com>
2024-05-25 09:13:09 +0800

    game logs are written to log.txt

--- Gobang_online_AG/src/logger.hpp
+++ Gobang_online_AG/src/logger.hpp
@@ -2,22 +2,32 @@
 
 #include <iostream>
 #include <ctime>
+#include <cstdio>
 
 #define INF 0
 #define DBG 1
 #define ERR 2
 #define DEFAULT_LOG_LEVEL INF
 
-#define LOG(level, format, ...)                                                             \
-    do                                                                                      \
-    {                                                                                       \
-        if (DEFAULT_LOG_LEVEL > level)                                                      \
-            break;                                                                          \
-        time_t t = time(NULL);                                                              \
-        struct tm *lt = localtime(&t);                                                      \
-        char buf[32] = {0};                                                                 \
-        strftime(buf, 31, "%H:%M:%S", lt);                                                  \
-        fprintf(stdout, "[%s %s:%d] " format "\n", buf, __FILE__, __LINE__, ##__VA_ARGS__); \
+#define LOG(level, format, ...)                                                                 \
+    do                                                                                          \
+    {                                                                                           \
+        if (DEFAULT_LOG_LEVEL > level)                                                          \
+            break;                                                                              \
+        time_t t = time(NULL);                                                                  \
+        struct tm *lt = localtime(&t);                                                          \
+        char buf[32] = {0};                                                                     \
+        strftime(buf, 31, "%H:%M:%S", lt);                                                      \
+        FILE* pf_log = fopen("./log.txt", "a");                                                 \
+        if (pf_log)                                                                             \
+        {                                                                                       \
+            fprintf(pf_log, "[%s %s:%d] " format "\n", buf, __FILE__, __LINE__, ##__VA_ARGS__); \
+            fclose(pf_log);                                                                     \
+        }                                                                                       \
+        else                                                                                    \
+        {                                                                                       \
+            fprintf(stderr, "Failed to open log file\n");                                       \
+        }                                                                                       \
     } while (false)
 
 #define ILOG(format, ...) LOG(INF, format, ##__VA_ARGS__)
